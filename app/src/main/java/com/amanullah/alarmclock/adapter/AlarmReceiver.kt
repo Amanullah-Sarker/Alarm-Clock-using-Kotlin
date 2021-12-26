9f22090ffc9dd6bb7835aaafd61647b4778f1bed
Amanullah Sarker <amanullahsarker3@gmail.com>
2021-12-26 22:35:58 +0600

    First Update

--- app/src/main/java/com/amanullah/alarmclock/adapter/AlarmReceiver.kt
+++ app/src/main/java/com/amanullah/alarmclock/adapter/AlarmReceiver.kt
@@ -9,7 +9,6 @@ import androidx.core.app.NotificationManagerCompat
 import com.amanullah.alarmclock.R
 import android.media.RingtoneManager
 import androidx.fragment.app.FragmentManager
-import com.amanullah.alarmclock.DestinationFragment
 import com.amanullah.alarmclock.activity.DestinationActivity
 
 
--- app/src/main/java/com/amanullah/alarmclock/fragment/DestinationFragment.kt
+++ app/src/main/java/com/amanullah/alarmclock/fragment/DestinationFragment.kt
@@ -1,4 +1,4 @@
-package com.amanullah.alarmclock
+package com.amanullah.alarmclock.fragment
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
